c4b3eb6ddfb879d88a4c2c1e42d330bf1f8e839e
tribta <joaocartuchoo@gmail.com>
2018-01-19 20:41:16 +0000

    added help messages

--- 3.run_this.py
+++ 3.run_this.py
@@ -12,11 +12,18 @@ def change_img_index(x):
     img_index = x
     img_path = image_list[img_index]
     img = cv2.imread(img_path)
+    cv2.displayOverlay(WINDOW_NAME, "Showing image "
+                                    "" + str(img_index) + "/"
+                                    "" + str(last_img_index), 1000)
 
 
 def change_class_index(x):
     global class_index
     class_index = x
+    cv2.displayOverlay(WINDOW_NAME, "Selected class "
+                                "" + str(class_index) + "/"
+                                "" + str(last_class_index) + ""
+                                "\n " + class_list[class_index],3000)
 
 
 def draw_edges(tmp_img):
@@ -44,12 +51,14 @@ def increase_index(current_index, last_index):
 
 # load img list
 image_list = glob.glob('1.insert_images_here/*')
-print(image_list)
+#print(image_list)
+last_img_index = len(image_list) - 1
 
 # load class list
 with open('2.insert_class_list_here.txt') as f:
     class_list = f.read().splitlines()
-print(class_list)
+#print(class_list)
+last_class_index = len(class_list) - 1
 
 # random rgb for each class
 class_rgb = np.random.random_integers(low=0, high=255, size=(3,len(class_list)))
@@ -61,15 +70,18 @@ cv2.resizeWindow(WINDOW_NAME, 1000, 600)
 
 # selected image
 TRACKBAR_IMG = 'Image'
-cv2.createTrackbar(TRACKBAR_IMG, WINDOW_NAME, 0, len(image_list) - 1, change_img_index)
+cv2.createTrackbar(TRACKBAR_IMG, WINDOW_NAME, 0, last_img_index, change_img_index)
 
 # selected class
 TRACKBAR_CLASS = 'Class'
-cv2.createTrackbar(TRACKBAR_CLASS, WINDOW_NAME, 0, len(class_list) - 1, change_class_index)
+cv2.createTrackbar(TRACKBAR_CLASS, WINDOW_NAME, 0, last_class_index, change_class_index)
 
 # initialize
 change_img_index(0)
 edges_on = False
+
+cv2.displayOverlay(WINDOW_NAME, "Welcome to fastbbox!\n Press [h] for help.", 4000)
+print(" Welcome to fastbbox!\n Select the window and press [h] for help.")
 
 # loop
 while True:
@@ -82,29 +94,38 @@ while True:
     cv2.imshow(WINDOW_NAME, tmp_img)
     pressed_key = cv2.waitKey(50)
 
-    # show previous image key listener
-    if pressed_key == ord('a'):
-        img_index = decrease_index(img_index, len(image_list) - 1)
+    if pressed_key == ord('a') or pressed_key == ord('d'):
+        # show previous image key listener
+        if pressed_key == ord('a'):
+            img_index = decrease_index(img_index, last_img_index)
+        # show next image key listener
+        elif pressed_key == ord('d'):
+            img_index = increase_index(img_index, last_img_index)
         cv2.setTrackbarPos(TRACKBAR_IMG, WINDOW_NAME, img_index)
-    # show next image key listener
-    elif pressed_key == ord('d'):
-        img_index = increase_index(img_index, len(image_list) - 1)
-        cv2.setTrackbarPos(TRACKBAR_IMG, WINDOW_NAME, img_index)
-    # change down current class key listener
-    elif pressed_key == ord('s'):
-        class_index = decrease_index(class_index, len(class_list) - 1)
+    elif pressed_key == ord('s') or pressed_key == ord('w'):
+        # change down current class key listener
+        if pressed_key == ord('s'):
+            class_index = decrease_index(class_index, last_class_index)
+        # change up current class key listener
+        elif pressed_key == ord('w'):
+            class_index = increase_index(class_index, last_class_index)
         cv2.setTrackbarPos(TRACKBAR_CLASS, WINDOW_NAME, class_index)
-    # change up current class key listener
-    elif pressed_key == ord('w'):
-        class_index = increase_index(class_index, len(class_list) - 1)
-        cv2.setTrackbarPos(TRACKBAR_CLASS, WINDOW_NAME, class_index)
+
+    # help key listener
+    elif pressed_key == ord('h'):
+        cv2.displayOverlay(WINDOW_NAME, "[e] to show edges;\n"
+                                "[q] to quit;\n"
+                                "[a] or [d] to change Image;\n"
+                                "[w] or [s] to change Class.", 6000)
     # show edges key listener
     elif pressed_key == ord('e'):
         if edges_on == True:
             edges_on = False
+            cv2.displayOverlay(WINDOW_NAME, "Edges turned OFF!", 1000)
         else:
+            cv2.displayOverlay(WINDOW_NAME, "Edges turned ON!", 1000)
             edges_on = True
-    # q - quit key listener
+    # quit key listener
     elif pressed_key == ord('q'):
         break
 
