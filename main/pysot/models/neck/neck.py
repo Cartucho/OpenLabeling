d5d505770ae6b720d7de61fc45d004cca06522a9
Sebastian Koch <koch.sebastian@mail.com>
2021-02-02 12:17:03 +0100

    add siammask from pysot

---
+++ main/pysot/models/neck/neck.py
@@ -0,0 +1,52 @@
+# Copyright (c) SenseTime. All Rights Reserved.
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import torch.nn as nn
+
+
+class AdjustLayer(nn.Module):
+    def __init__(self, in_channels, out_channels, center_size=7):
+        super(AdjustLayer, self).__init__()
+        self.downsample = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            )
+        self.center_size = center_size
+
+    def forward(self, x):
+        x = self.downsample(x)
+        if x.size(3) < 20:
+            l = (x.size(3) - self.center_size) // 2
+            r = l + self.center_size
+            x = x[:, :, l:r, l:r]
+        return x
+
+
+class AdjustAllLayer(nn.Module):
+    def __init__(self, in_channels, out_channels, center_size=7):
+        super(AdjustAllLayer, self).__init__()
+        self.num = len(out_channels)
+        if self.num == 1:
+            self.downsample = AdjustLayer(in_channels[0],
+                                          out_channels[0],
+                                          center_size)
+        else:
+            for i in range(self.num):
+                self.add_module('downsample'+str(i+2),
+                                AdjustLayer(in_channels[i],
+                                            out_channels[i],
+                                            center_size))
+
+    def forward(self, features):
+        if self.num == 1:
+            return self.downsample(features)
+        else:
+            out = []
+            for i in range(self.num):
+                adj_layer = getattr(self, 'downsample'+str(i+2))
+                out.append(adj_layer(features[i]))
+            return out
---
+++ main/siammask.py
@@ -0,0 +1,127 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import os
+import argparse
+import sys
+import cv2
+import torch
+import numpy as np
+from glob import glob
+
+from pysot.core.config import cfg
+from pysot.models.model_builder import ModelBuilder
+from pysot.tracker.tracker_builder import build_tracker
+
+from os.path import realpath, dirname, join, exists
+# set device, depending on whether cuda is available
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+
+
+class SiamMask(object):
+
+
+    def __init__(self,classid=0,anchorid=0,init_frame=None,init_bbox=None):
+        # load config
+        cfg_path = 'pysot/experiments/siamrpn_r50_l234_dwxcorr/config.yaml'
+        snapshot = 'pysot/experiments/siamrpn_r50_l234_dwxcorr/model.pth'
+        cfg.merge_from_file(cfg_path)
+        cfg.CUDA = torch.cuda.is_available() and cfg.CUDA
+        # cfg.CUDA = False
+        device = torch.device('cuda' if cfg.CUDA else 'cpu')
+        # device='cpu'
+
+        # create model
+        model = ModelBuilder()
+
+        # load model
+        model.load_state_dict(torch.load(snapshot,
+            map_location=lambda storage, loc: storage.cpu()))
+        model.eval().to(device)
+
+        # build tracker
+        tracker = build_tracker(model)
+        
+        self.tracker = tracker
+        self.classId = classid
+        self.anchorId = anchorid
+        self.init_bbox = init_bbox
+        self.init_frame = init_frame
+            
+
+    def init(self, init_frame, initial_bbox):
+        """
+        Initialize DaSiamRPN tracker with inital frame and bounding box.
+        """
+        with torch.no_grad():
+            self.tracker.init(init_frame,initial_bbox)
+        # target_pos, target_sz = self.bbox_to_pos(initial_bbox)
+        # self.state = SiamRPN_init(
+        #     init_frame, target_pos, target_sz, self.net)
+
+    def init_reinit(self):
+        self.init(self.init_frame,self.init_bbox)
+
+    def update(self, next_image):
+        """
+        Update bounding box position and size on next_image. Returns True
+        beacuse tracking is terminated based on number of frames predicted
+        in OpenLabeling, not based on feedback from tracking algorithm (unlike
+        the opencv tracking algorithms).
+        """
+        with torch.no_grad():
+            # self.state = SiamRPN_track(self.state, next_image)
+            # target_pos = self.state["target_pos"]
+            # target_sz  = self.state["target_sz"]
+            # bbox = self.pos_to_bbox(target_pos, target_sz)
+            outputs = self.tracker.track(next_image)
+            # if 'polygon' in outputs:
+            #     polygon = np.array(outputs['polygon']).astype(np.int32)
+            #     cv2.polylines(frame, [polygon.reshape((-1, 1, 2))],
+            #                     True, (0, 255, 0), 3)
+            #     mask = ((outputs['mask'] > cfg.TRACK.MASK_THERSHOLD) * 255)
+            #     mask = mask.astype(np.uint8)
+            #     mask = np.stack([mask, mask*255, mask]).transpose(1, 2, 0)
+            #     frame = cv2.addWeighted(frame, 0.77, mask, 0.23, -1)
+            # else:
+            bbox = list(map(int, outputs['bbox']))
+            # cv2.rectangle(frame, (bbox[0], bbox[1]),
+            #                 (bbox[0]+bbox[2], bbox[1]+bbox[3]),
+            #                 (0, 255, 0), 3)
+            score = outputs['best_score']
+            if score >0.6:
+                return True, bbox
+            else:
+                return False, bbox
+
+            # return True, bbox
+
+    def bbox_to_pos(self, initial_bbox):
+        """
+        Convert bounding box format from a tuple format containing
+        xmin, ymin, width, and height to a tuple of two arrays which contain
+        the x and y coordinates of the center of the box and its width and
+        height respectively.
+        """
+        xmin, ymin, w, h = initial_bbox
+        cx = int(xmin + w/2)
+        cy = int(ymin + h/2)
+        target_pos = np.array([cx, cy])
+        target_sz  = np.array([w, h])
+
+        return target_pos, target_sz
+
+    def pos_to_bbox(self, target_pos, target_sz):
+        """
+        Invert the bounding box format produced in the above conversion
+        function.
+        """
+        w = target_sz[0]
+        h = target_sz[1]
+        xmin = int(target_pos[0] - w/2)
+        ymin = int(target_pos[1] - h/2)
+
+        return xmin, ymin, w, h
